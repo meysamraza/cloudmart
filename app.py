637ed3ae2da1ccac2ea54db4203daa98cd5b0826
meysam <arazameysa@gmail.com>
2026-09-21 10:00:04 +0500

    sast scan added

--- app.py
+++ app.py
@@ -449,4 +449,4 @@ if __name__ == "__main__":
     # Python code execution when unhandled exceptions occur.
     print("[*] Starting CloudMart Vulnerable Application...")
     print("[*] Web Dashboard: http://127.0.0.1:5000")
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
